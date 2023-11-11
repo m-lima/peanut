c070d90b83fc2453a11aeaf017df77d63930285a
Marcelo Lima <m-lima@users.noreply.github.com>
2023-11-12 00:11:10 +0100

    Test round trips

--- src/main.rs
+++ src/main.rs
@@ -23,15 +23,25 @@ fn main() -> std::process::ExitCode {
 
 fn fallible_main(command: args::Command) -> anyhow::Result<()> {
     match command {
-        args::Command::Encrypt(_) => stream(
-            std::io::stdin().lock(),
-            zstd::Encoder::new(std::io::stdout().lock(), 9)?.auto_finish(),
-        ),
-        args::Command::Decrypt(_) => stream(
-            zstd::Decoder::new(std::io::stdin().lock())?,
-            std::io::stdout().lock(),
-        ),
+        args::Command::Encrypt(_) => encrypt(std::io::stdin().lock(), std::io::stdout().lock()),
+        args::Command::Decrypt(_) => decrypt(std::io::stdin().lock(), std::io::stdout().lock()),
     }
+}
+
+fn encrypt<Input, Output>(input: Input, output: Output) -> anyhow::Result<()>
+where
+    Input: std::io::Read,
+    Output: std::io::Write,
+{
+    stream(input, zstd::Encoder::new(output, 9)?.auto_finish())
+}
+
+fn decrypt<Input, Output>(input: Input, output: Output) -> anyhow::Result<()>
+where
+    Input: std::io::Read,
+    Output: std::io::Write,
+{
+    stream(zstd::Decoder::new(input)?, output)
 }
 
 fn stream<Input, Output>(mut input: Input, mut output: Output) -> anyhow::Result<()>
@@ -59,5 +69,38 @@ where
         if let Err(err) = output.write_all(&buf[..bytes]) {
             error!("Error while writing to stdout: {err}");
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn transparent_round_trip() {
+        let input = (u8::MIN..=u8::MAX)
+            .flat_map(|_| (u8::MIN..u8::MAX))
+            .collect::<Vec<_>>();
+
+        let mut output = Vec::with_capacity(usize::from(u8::MAX) * usize::from(u8::MAX));
+
+        stream(input.as_slice(), &mut output).unwrap();
+
+        assert_eq!(input, output);
+    }
+
+    #[test]
+    fn round_trip() {
+        let input = (u8::MIN..=u8::MAX)
+            .flat_map(|_| (u8::MIN..u8::MAX))
+            .collect::<Vec<_>>();
+
+        let mut transient = Vec::with_capacity(usize::from(u8::MAX) * usize::from(u8::MAX));
+        let mut output = Vec::with_capacity(usize::from(u8::MAX) * usize::from(u8::MAX));
+
+        encrypt(input.as_slice(), &mut transient).unwrap();
+        decrypt(transient.as_slice(), &mut output).unwrap();
+
+        assert_eq!(input, output);
     }
 }
