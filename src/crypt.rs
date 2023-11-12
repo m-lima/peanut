41b75e43e3940bf77f9b9b4383104fbbf01e7736
Marcelo Lima <m-lima@users.noreply.github.com>
2023-11-12 20:51:15 +0100

    WIP: Broken decryption

---
+++ src/crypt.rs
@@ -0,0 +1,225 @@
+use super::BUF_LEN;
+const TAG_LEN: usize = 16;
+
+pub struct Cryptor<Out>
+where
+    Out: std::io::Write,
+{
+    // TODO: This option here is kinda ridiculous
+    stream: Option<aead::stream::EncryptorBE32<aes_gcm_siv::Aes256GcmSiv>>,
+    buffer: aead::arrayvec::ArrayVec<u8, BUF_LEN>,
+    output: Out,
+}
+
+impl<Out> Cryptor<Out>
+where
+    Out: std::io::Write,
+{
+    pub fn new(key: [u8; 32], mut output: Out) -> anyhow::Result<Self> {
+        use aead::KeyInit;
+        use anyhow::Context;
+
+        let nonce = make_nonce();
+        let key = derive_key(key, nonce);
+
+        output.write_all(&nonce).context("Could not write nonce")?;
+
+        let stream = Some(aead::stream::EncryptorBE32::from_aead(
+            aes_gcm_siv::Aes256GcmSiv::new(key.as_slice().into()),
+            [0; 7].as_slice().into(),
+        ));
+        let buffer = aead::arrayvec::ArrayVec::new();
+
+        Ok(Self {
+            stream,
+            buffer,
+            output,
+        })
+    }
+
+    fn finish(&mut self) -> anyhow::Result<()> {
+        use anyhow::Context;
+
+        if !self.buffer.is_empty() {
+            self.stream
+                .take()
+                .unwrap()
+                .encrypt_last_in_place(b"", &mut self.buffer)
+                .map_err(|err| anyhow::anyhow!("Could not encrypt the last block: {err}"))?;
+
+            self.output
+                .write_all(&self.buffer)
+                .context("Could not write the last block")?;
+        }
+        self.output
+            .flush()
+            .context("Could not flush the last block")
+    }
+}
+
+impl<Out> std::io::Write for Cryptor<Out>
+where
+    Out: std::io::Write,
+{
+    fn write(&mut self, mut buf: &[u8]) -> std::io::Result<usize> {
+        use aead::Buffer;
+
+        const MAX_CAP: usize = BUF_LEN - TAG_LEN;
+
+        let mut sent = 0;
+        let mut capacity = MAX_CAP - self.buffer.len();
+
+        while buf.len() > capacity {
+            self.buffer
+                .extend_from_slice(&buf[..capacity])
+                .map_err(|err| {
+                    std::io::Error::new(std::io::ErrorKind::OutOfMemory, err.to_string())
+                })?;
+
+            self.stream
+                .as_mut()
+                .unwrap()
+                .encrypt_next_in_place(b"", &mut self.buffer)
+                .map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err.to_string()))?;
+
+            self.output.write_all(&self.buffer)?;
+
+            self.buffer.clear();
+            buf = &buf[capacity..];
+            sent += capacity;
+            capacity = MAX_CAP;
+        }
+
+        self.buffer
+            .extend_from_slice(buf)
+            .map_err(|err| std::io::Error::new(std::io::ErrorKind::OutOfMemory, err.to_string()))?;
+
+        Ok(sent + self.buffer.len())
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        if !self.buffer.is_empty() {
+            self.stream
+                .as_mut()
+                .unwrap()
+                .encrypt_next_in_place(b"", &mut self.buffer)
+                .map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err.to_string()))?;
+
+            self.output.write_all(&self.buffer)?;
+        }
+        self.output.flush()
+    }
+}
+
+impl<Out> Drop for Cryptor<Out>
+where
+    Out: std::io::Write,
+{
+    fn drop(&mut self) {
+        if let Err(err) = self.finish() {
+            error!("Failed to drop Cryptor: {err:?}");
+        }
+    }
+}
+
+pub struct Decryptor<In>
+where
+    In: std::io::Read,
+{
+    stream: Option<aead::stream::DecryptorBE32<aes_gcm_siv::Aes256GcmSiv>>,
+    buffer: aead::arrayvec::ArrayVec<u8, BUF_LEN>,
+    cursor: usize,
+    input: In,
+}
+
+impl<In> Decryptor<In>
+where
+    In: std::io::Read,
+{
+    pub fn new(key: [u8; 32], mut input: In) -> anyhow::Result<Self> {
+        use aead::KeyInit;
+        use anyhow::Context;
+
+        let mut nonce = [0; 24];
+        input
+            .read_exact(&mut nonce)
+            .context("Could not read nonce")?;
+
+        let key = derive_key(key, nonce);
+
+        let stream = Some(aead::stream::DecryptorBE32::from_aead(
+            aes_gcm_siv::Aes256GcmSiv::new(key.as_slice().into()),
+            [0; 7].as_slice().into(),
+        ));
+        let buffer = aead::arrayvec::ArrayVec::new();
+
+        Ok(Self {
+            stream,
+            buffer,
+            cursor: 0,
+            input,
+        })
+    }
+}
+
+impl<In> std::io::Read for Decryptor<In>
+where
+    In: std::io::Read,
+{
+    fn read(&mut self, mut buf: &mut [u8]) -> std::io::Result<usize> {
+        let mut read = 0;
+        while !buf.is_empty() {
+            let buf_size = self.buffer.len() - self.cursor;
+            if buf_size > 0 {
+                let len = buf_size.min(buf.len());
+                buf[..len].copy_from_slice(&self.buffer[self.cursor..self.cursor + len]);
+                self.cursor += len;
+                buf = &mut buf[len..];
+                read += len;
+
+                continue;
+            }
+
+            if self.stream.is_none() {
+                break;
+            }
+
+            self.cursor = 0;
+            unsafe {
+                self.buffer.set_len(BUF_LEN);
+                let read = self.input.read(&mut self.buffer)?;
+                self.buffer.set_len(read);
+            };
+
+            if self.buffer.len() < BUF_LEN {
+                self.stream
+                    .take()
+                    .unwrap()
+                    .decrypt_last_in_place(b"", &mut self.buffer)
+                    .map_err(|err| {
+                        std::io::Error::new(std::io::ErrorKind::Other, err.to_string())
+                    })?;
+            } else {
+                self.stream
+                    .as_mut()
+                    .unwrap()
+                    .decrypt_next_in_place(b"", &mut self.buffer)
+                    .map_err(|err| {
+                        std::io::Error::new(std::io::ErrorKind::Other, err.to_string())
+                    })?;
+            }
+        }
+        Ok(read)
+    }
+}
+
+fn make_nonce() -> [u8; 24] {
+    let mut nonce =
+        unsafe { std::mem::transmute::<_, [u8; 24]>([std::mem::MaybeUninit::<u8>::uninit(); 24]) };
+    aead::rand_core::RngCore::fill_bytes(&mut aead::OsRng, &mut nonce);
+    nonce
+}
+
+fn derive_key(key: [u8; 32], nonce: [u8; 24]) -> [u8; 32] {
+    nonce_extension::nonce_extension_aes256(key, nonce)
+}
--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ macro_rules! error {
 }
 
 mod args;
+mod crypt;
 
 const BUF_LEN: usize = 8 * 1024;
-const TAG_LEN: usize = 16;
 
 fn main() -> std::process::ExitCode {
     if let Err(err) = fallible_main() {
@@ -30,7 +30,7 @@ fn fallible_main() -> anyhow::Result<()> {
 
     match command {
         args::Command::Encrypt(key) => encrypt(key, stdin, stdout),
-        args::Command::Decrypt(_) => decrypt(stdin, stdout),
+        args::Command::Decrypt(key) => decrypt(key, stdin, stdout),
     }
 }
 
@@ -39,166 +39,17 @@ where
     Input: std::io::Read,
     Output: std::io::Write,
 {
-    let encryptor = Cryptor::new(key, output)?;
+    let encryptor = crypt::Cryptor::new(key, output)?;
     stream(input, zstd::Encoder::new(encryptor, 9)?.auto_finish())
 }
 
-struct Cryptor<Out>
-where
-    Out: std::io::Write,
-{
-    stream: Option<aead::stream::EncryptorLE31<aes_gcm_siv::Aes256GcmSiv>>,
-    buffer: aead::arrayvec::ArrayVec<u8, BUF_LEN>,
-    out: Out,
-}
-
-impl<Out> Cryptor<Out>
-where
-    Out: std::io::Write,
-{
-    fn new(key: [u8; 32], mut out: Out) -> anyhow::Result<Self> {
-        use aead::KeyInit;
-        use anyhow::Context;
-
-        let nonce = make_nonce();
-        let key = derive_key(key, nonce);
-
-        out.write_all(&nonce).context("Could not write nonce")?;
-
-        let stream = Some(aead::stream::EncryptorLE31::from_aead(
-            aes_gcm_siv::Aes256GcmSiv::new(key.as_slice().into()),
-            [0; 8].as_slice().into(),
-        ));
-        let buffer = aead::arrayvec::ArrayVec::new();
-
-        Ok(Self {
-            stream,
-            buffer,
-            out,
-        })
-    }
-
-    fn finish(&mut self) -> anyhow::Result<()> {
-        use anyhow::Context;
-
-        if !self.buffer.is_empty() {
-            self.stream
-                .take()
-                .unwrap()
-                .encrypt_last_in_place(b"", &mut self.buffer)
-                .map_err(|err| anyhow::anyhow!("Could not encrypt the last block: {err}"))?;
-
-            self.out
-                .write_all(&self.buffer)
-                .context("Could not write the last block")?;
-        }
-        self.out.flush().context("Could not flush the last block")
-    }
-}
-
-impl<Out> std::io::Write for Cryptor<Out>
-where
-    Out: std::io::Write,
-{
-    fn write(&mut self, mut buf: &[u8]) -> std::io::Result<usize> {
-        use aead::Buffer;
-
-        const MAX_CAP: usize = BUF_LEN - TAG_LEN;
-
-        let mut sent = 0;
-        let mut capacity = MAX_CAP - self.buffer.len();
-
-        // let id = *buf.first().unwrap_or(&0);
-
-        // eprintln!(
-        //     "{id} Write :: Len: {len}, Cap: {capacity}, Buf: {buf}",
-        //     len = self.buffer.len(),
-        //     buf = buf.len()
-        // );
-
-        while buf.len() > capacity {
-            // eprintln!("{id} Need to flush");
-            self.buffer
-                .extend_from_slice(&buf[..capacity])
-                .map_err(|err| {
-                    std::io::Error::new(std::io::ErrorKind::OutOfMemory, err.to_string())
-                })?;
-
-            // eprintln!(
-            //     "{id} Extend :: Len: {len}, Cap: {capacity}, Buf: {buf}, Snt: {sent}",
-            //     len = self.buffer.len(),
-            //     buf = buf.len()
-            // );
-
-            self.stream
-                .as_mut()
-                .unwrap()
-                .encrypt_next_in_place(b"", &mut self.buffer)
-                .map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err.to_string()))?;
-
-            self.out.write_all(&self.buffer)?;
-
-            self.buffer.clear();
-            buf = &buf[capacity..];
-            sent += capacity;
-            capacity = MAX_CAP;
-
-            // eprintln!(
-            //     "{id} Rotate :: Len: {len}, Cap: {capacity}, Buf: {buf}, Snt: {sent}",
-            //     len = self.buffer.len(),
-            //     buf = buf.len()
-            // );
-        }
-
-        self.buffer
-            .extend_from_slice(buf)
-            .map_err(|err| std::io::Error::new(std::io::ErrorKind::OutOfMemory, err.to_string()))?;
-
-        Ok(sent + self.buffer.len())
-    }
-
-    fn flush(&mut self) -> std::io::Result<()> {
-        if !self.buffer.is_empty() {
-            self.stream
-                .as_mut()
-                .unwrap()
-                .encrypt_next_in_place(b"", &mut self.buffer)
-                .map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err.to_string()))?;
-
-            self.out.write_all(&self.buffer)?;
-        }
-        self.out.flush()
-    }
-}
-
-impl<Out> Drop for Cryptor<Out>
-where
-    Out: std::io::Write,
-{
-    fn drop(&mut self) {
-        if let Err(err) = self.finish() {
-            error!("Failed to drop Cryptor: {err:?}");
-        }
-    }
-}
-
-fn decrypt<Input, Output>(input: Input, output: Output) -> anyhow::Result<()>
+fn decrypt<Input, Output>(key: [u8; 32], input: Input, output: Output) -> anyhow::Result<()>
 where
     Input: std::io::Read,
     Output: std::io::Write,
 {
-    stream(zstd::Decoder::new(input)?, output)
-}
-
-fn make_nonce() -> [u8; 24] {
-    let mut nonce =
-        unsafe { std::mem::transmute::<_, [u8; 24]>([std::mem::MaybeUninit::<u8>::uninit(); 24]) };
-    aead::rand_core::RngCore::fill_bytes(&mut aead::OsRng, &mut nonce);
-    nonce
-}
-
-fn derive_key(key: [u8; 32], nonce: [u8; 24]) -> [u8; 32] {
-    nonce_extension::nonce_extension_aes256(key, nonce)
+    let decryptor = crypt::Decryptor::new(key, input)?;
+    stream(zstd::Decoder::new(decryptor)?, output)
 }
 
 fn stream<Input, Output>(mut input: Input, mut output: Output) -> anyhow::Result<()>
@@ -254,7 +105,7 @@ mod tests {
         let mut output = Vec::with_capacity(usize::from(u8::MAX) * usize::from(u8::MAX));
 
         encrypt([0; 32], input.as_slice(), &mut transient).unwrap();
-        decrypt(transient.as_slice(), &mut output).unwrap();
+        decrypt([0; 32], transient.as_slice(), &mut output).unwrap();
 
         assert_eq!(input, output);
     }
