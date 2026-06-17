99a0c190ad56340a35f6968f45dfe09ca500c330
Marcelo Lima <m-lima@users.noreply.github.com>
2026-06-17 10:53:56 +0200

    Update dependencies

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     crane.url = "github:ipetkov/crane";
     fenix = {
       url = "github:nix-community/fenix";
